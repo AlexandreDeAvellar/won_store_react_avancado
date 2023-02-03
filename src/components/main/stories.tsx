992fa3d7653c919f9dc6b891fab7a798ab0044a9
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2023-02-03 13:49:10 -0300

    chore: add controls config of storybook and plop to generate template

--- src/components/main/stories.tsx
+++ src/components/main/stories.tsx
@@ -1,8 +1,14 @@
 import Main from '.'
+import { Story, Meta } from '@storybook/react'
 
 export default {
   title: 'Main',
   component: Main
-}
+} as Meta
 
-export const Basic = () => <Main />
+export const Basic: Story = (args) => <Main {...args} />
+
+Basic.args = {
+  title: 'React Avançado',
+  description: 'TypeScript, ReactJs, NextJs e Styled-Components'
+}
