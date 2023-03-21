4b3a9342a1e495552da7591fb864c3193f57cd66
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2023-03-21 08:04:20 -0300

    test: ensure Heading render a heading with a line at the bottom

--- src/components/Heading/test.tsx
+++ src/components/Heading/test.tsx
@@ -1,5 +1,6 @@
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from '../../utils/tests/helpers'
+import 'jest-styled-components'
 import Heading from '.'
 
 describe('<Heading />', () => {
@@ -22,5 +23,16 @@ describe('<Heading />', () => {
     expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
       'border-left': '0.7rem solid #3CD3C1'
     })
+  })
+
+  it('should render a heading with a line at the bottom', () => {
+    renderWithTheme(<Heading lineBottom>Won Games</Heading>)
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyleRule(
+      'width',
+      '5rem',
+      {
+        modifier: '::after'
+      }
+    )
   })
 })
