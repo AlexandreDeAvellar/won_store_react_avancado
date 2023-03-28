21d65dddd99da63f37c7fce9845c40fac3ff81c0
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2023-03-28 13:43:15 -0300

    test: ensure Menu component render correcly in each change

---
+++ src/components/Menu/test.tsx
@@ -0,0 +1,48 @@
+import { fireEvent, screen } from '@testing-library/react'
+import { renderWithTheme } from '../../utils/tests/helpers'
+import Menu from '.'
+
+describe('<Menu />', () => {
+  it('should render the menu', () => {
+    renderWithTheme(<Menu />)
+
+    expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/won games/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/search/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/open shopping cart/i)).toBeInTheDocument()
+  })
+
+  it('should handle the open/closed mobile menu', () => {
+    renderWithTheme(<Menu />)
+    const renderMenuFull = screen.getByRole('navigation', { hidden: true })
+
+    expect(renderMenuFull.getAttribute('aria-hidden')).toBe('true')
+    expect(renderMenuFull).toHaveStyle({ opacity: '0' })
+
+    fireEvent.click(screen.getByLabelText(/open menu/i))
+    expect(renderMenuFull.getAttribute('aria-hidden')).toBe('false')
+    expect(renderMenuFull).toHaveStyle({ opacity: '1' })
+
+    fireEvent.click(screen.getByLabelText(/close menu/i))
+    expect(renderMenuFull.getAttribute('aria-hidden')).toBe('true')
+    expect(renderMenuFull).toHaveStyle({ opacity: '0' })
+  })
+
+  it('should show register box when logged out', () => {
+    renderWithTheme(<Menu />)
+
+    expect(screen.queryByText(/wishlist/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/my account/i)).not.toBeInTheDocument()
+    expect(screen.getByText(/log in now/i)).toBeInTheDocument()
+    expect(screen.getByText(/sign up/i)).toBeInTheDocument()
+  })
+
+  it('should show wishlist and account when logged in', () => {
+    renderWithTheme(<Menu username="any_name" />)
+
+    expect(screen.getByText(/wishlist/i)).toBeInTheDocument()
+    expect(screen.getByText(/my account/i)).toBeInTheDocument()
+    expect(screen.queryByText(/log in now/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/sign up/i)).not.toBeInTheDocument()
+  })
+})
