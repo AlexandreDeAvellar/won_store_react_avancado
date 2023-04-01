74f17b379d48b9aaed9e378a8cc775b6875e048a
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2023-04-01 12:06:11 -0300

    test: ensure Menu component render correctly sign in button if logged or not

--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -35,6 +35,7 @@ describe('<Menu />', () => {
     expect(screen.queryByText(/my account/i)).not.toBeInTheDocument()
     expect(screen.getByText(/log in now/i)).toBeInTheDocument()
     expect(screen.getByText(/sign up/i)).toBeInTheDocument()
+    expect(screen.queryByText(/sign in/i)).toBeInTheDocument()
   })
 
   it('should show wishlist and account when logged in', () => {
@@ -44,5 +45,6 @@ describe('<Menu />', () => {
     expect(screen.getByText(/my account/i)).toBeInTheDocument()
     expect(screen.queryByText(/log in now/i)).not.toBeInTheDocument()
     expect(screen.queryByText(/sign up/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/sign in/i)).not.toBeInTheDocument()
   })
 })
