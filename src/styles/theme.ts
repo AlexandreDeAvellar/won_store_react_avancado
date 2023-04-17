63b8985fa559263ed566e781cc26d90b729d462d
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2023-04-17 08:24:27 -0300

    feat: add lightBg in theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -7,8 +7,7 @@ export default {
     radius: '0.4rem'
   },
   font: {
-    family:
-      "Poppins, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+    family: "Poppins, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
     light: 300,
     normal: 400,
     bold: 600,
@@ -27,6 +26,7 @@ export default {
     mainBg: '#06092B',
     white: '#FAFAFA',
     black: '#030517',
+    lightBg: '#F2F2F2',
     lightGray: '#EAEAEA',
     gray: '#8F8F8F',
     darkGray: '#2E2F42'
