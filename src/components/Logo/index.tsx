3875fe9bca86227f5c33b30746772b208fe2e25f
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2023-04-26 08:52:46 -0300

    feat: update logo icon to receive id when necessary

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -5,11 +5,12 @@ export type LogoProps = {
   color?: 'white' | 'black'
   size?: 'normal' | 'large'
   hideOnMobile?: boolean
+  id?: string
 }
 
-const Logo = ({ color = 'white', size = 'normal', hideOnMobile = false }: LogoProps) => (
+const Logo = ({ color = 'white', size = 'normal', hideOnMobile = false, id }: LogoProps) => (
   <S.Wrapper color={color} size={size} hideOnMobile={hideOnMobile}>
-    {controlIcon}
+    {controlIcon(id)}
   </S.Wrapper>
 )
 export default Logo
--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -11,6 +11,11 @@ describe('<Logo />', () => {
     })
   })
 
+  it('should render logo with id passed', () => {
+    const { container } = renderWithTheme(<Logo id="new_id" />)
+    expect(container.querySelector('#paint_linear_new_id')).toBeInTheDocument()
+  })
+
   it('should render a black label when color is passed', () => {
     renderWithTheme(<Logo color="black" />)
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
