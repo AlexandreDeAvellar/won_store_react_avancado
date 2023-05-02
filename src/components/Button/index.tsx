3c5c21a349e6720b8a5043ca431074dfa60fa5bb
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2023-05-02 08:09:40 -0300

    feat: update Button component to receive forwardRef

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,4 +1,4 @@
-import { AnchorHTMLAttributes, ButtonHTMLAttributes } from 'react'
+import React, { AnchorHTMLAttributes, ButtonHTMLAttributes } from 'react'
 import * as S from './styles'
 
 type ButtonTypes = AnchorHTMLAttributes<HTMLAnchorElement> | ButtonHTMLAttributes<HTMLButtonElement>
@@ -11,10 +11,16 @@ export type ButtonProps = {
   as?: React.ElementType
 } & ButtonTypes
 
-const Button = ({ children, size = 'medium', fullWidth = false, icon, minimal = false, ...args }: ButtonProps) => (
-  <S.Wrapper size={size} fullWidth={fullWidth} hasIcon={!!icon} minimal={minimal} {...args}>
-    {icon}
-    {!!children && <span>{children}</span>}
-  </S.Wrapper>
-)
-export default Button
+const Button: React.ForwardRefRenderFunction<S.WrapperProps, ButtonProps> = (
+  { children, size = 'medium', fullWidth = false, icon, minimal = false, ...args },
+  ref
+) => {
+  return (
+    <S.Wrapper size={size} fullWidth={fullWidth} hasIcon={!!icon} minimal={minimal} ref={ref} {...args}>
+      {icon}
+      {!!children && <span>{children}</span>}
+    </S.Wrapper>
+  )
+}
+
+export default React.forwardRef(Button)
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -2,7 +2,7 @@ import styled, { DefaultTheme, css } from 'styled-components'
 import { ButtonProps } from '.'
 import { darken } from 'polished'
 
-type WrapperProps = {
+export type WrapperProps = {
   hasIcon: boolean
 } & Pick<ButtonProps, 'size' | 'fullWidth' | 'minimal'>
 
