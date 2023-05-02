f720acc98d3610eb229158fc9791618d3884a8c5
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2023-05-02 08:16:18 -0300

    feat: create page SignUp

---
+++ src/pages/sign-up/index.tsx
@@ -0,0 +1,6 @@
+import FormSignUp from '../../components/FormSignUp'
+import Auth from '../../templates/Auth'
+
+const SignUp = () => <Auth title="Sign Up">{<FormSignUp />}</Auth>
+
+export default SignUp
