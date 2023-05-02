ca09bade02cd5f70db37d8d8751fe83d8ce33c2c
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2023-05-02 08:16:39 -0300

    feat: create page SignIn

---
+++ src/pages/sign-in/index.tsx
@@ -0,0 +1,6 @@
+import FormSignIn from '../../components/FormSignIn'
+import Auth from '../../templates/Auth'
+
+const SignIn = () => <Auth title="Sign In">{<FormSignIn />}</Auth>
+
+export default SignIn
