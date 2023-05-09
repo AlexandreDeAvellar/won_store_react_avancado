a290e97ea401ab67a33656f15252197300a89a39
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2023-05-09 07:59:27 -0300

    test: update Menu test to find sign in index 0

--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -33,9 +33,8 @@ describe('<Menu />', () => {
 
     expect(screen.queryByText(/wishlist/i)).not.toBeInTheDocument()
     expect(screen.queryByText(/my account/i)).not.toBeInTheDocument()
-    expect(screen.getByText(/log in now/i)).toBeInTheDocument()
     expect(screen.getByText(/sign up/i)).toBeInTheDocument()
-    expect(screen.queryByText(/sign in/i)).toBeInTheDocument()
+    expect(screen.queryAllByText(/sign in/i)[0]).toBeInTheDocument()
   })
 
   it('should show wishlist and account when logged in', () => {
@@ -43,7 +42,6 @@ describe('<Menu />', () => {
 
     expect(screen.getByText(/wishlist/i)).toBeInTheDocument()
     expect(screen.getByText(/my account/i)).toBeInTheDocument()
-    expect(screen.queryByText(/log in now/i)).not.toBeInTheDocument()
     expect(screen.queryByText(/sign up/i)).not.toBeInTheDocument()
     expect(screen.queryByText(/sign in/i)).not.toBeInTheDocument()
   })
