4ae05345f14ffaaed11f123e83a11a4b2a80e04b
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2023-06-10 08:18:31 -0300

    feat: add serverSideProps in Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,14 @@
-import Home from '../templates/Home'
+import Home, { HomeTemplateProps } from '../templates/Home'
 import { homeTemplateProps } from '../templates/Home/home-mocks'
 
-export default function Index() {
-  return <Home {...homeTemplateProps} />
+export default function Index(props: HomeTemplateProps) {
+  return <Home {...props} />
+}
+
+export async function getServerSideProps() {
+  return {
+    props: {
+      ...homeTemplateProps
+    }
+  }
 }
