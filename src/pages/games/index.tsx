421961ba228d039eb5eeac25378706d867752e3b
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2023-06-10 08:19:13 -0300

    feat: initializae apollo in server side of GamePage

--- src/pages/games/index.tsx
+++ src/pages/games/index.tsx
@@ -1,15 +1,20 @@
 import GamesTemplate, { GamesTemplateProps } from '../../templates/Games'
-import { gameCardProps } from '../../components/GameCardSlider/game-card-slider-mocks'
 import { explorerSidebarItemProps } from '../../components/ExploreSidebar/explorer-sidebar-mocks'
+import { initializeApollo } from '../../utils/apollo'
+import { QUERY_GAMES } from '../../graphql/queries/games'
+import { gameCardTransform } from '../../utils/graphql-transform/games'
 
 export default function GamesPage(props: GamesTemplateProps) {
   return <GamesTemplate {...props} />
 }
 
 export async function getServerSideProps() {
+  const apoloClient = initializeApollo()
+  const { data } = await apoloClient.query({ query: QUERY_GAMES, variables: { limit: 4 } })
+
   return {
     props: {
-      games: gameCardProps,
+      games: gameCardTransform(data?.games?.data),
       filterItems: explorerSidebarItemProps
     }
   }
