577fed1424e2e49930b04ade0321ac4b4da888fb
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2023-06-10 08:17:33 -0300

    feat: apply ApolloProvider in _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,27 @@
-import 'styles/globals.css'
-import GlobalStyles from 'styles/global'
-import type { AppProps } from 'next/app'
-import Head from 'next/head'
+import { ApolloProvider } from '@apollo/client'
 import { ThemeProvider } from 'styled-components'
+import type { AppProps } from 'next/app'
+
+import Head from 'next/head'
+
 import theme from '../styles/theme'
+import GlobalStyles from 'styles/global'
+import { useApollo } from '../utils/apollo'
+import 'styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const client = useApollo(pageProps.initialApolloState)
+
   return (
-    <ThemeProvider theme={theme}>
-      <Head>
-        <title>Won Games</title>
-        <meta name="description" content="The best Game Store in the world!" />
-      </Head>
-      <GlobalStyles />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <Head>
+          <title>Won Games</title>
+          <meta name="description" content="The best Game Store in the world!" />
+        </Head>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ApolloProvider>
   )
 }
