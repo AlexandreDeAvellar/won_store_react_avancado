06f4605343e3f722dbb75f6e70260818cd5393e5
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2023-06-21 12:33:50 -0300

    feat: create game by slug graphql query

---
+++ src/graphql/queries/game-by-slug.tsx
@@ -0,0 +1,61 @@
+import { gql } from '@apollo/client'
+
+export const QUERY_GAME_BY_SLUG = gql`
+  query QueryGameBySlug($slug: String!) {
+    games(filters: { slug: { eq: $slug } }) {
+      data {
+        attributes {
+          name
+          short_description
+          description
+          price
+          rating
+          release_date
+          gallery {
+            data {
+              attributes {
+                src: url
+                label: alternativeText
+              }
+            }
+          }
+          cover {
+            data {
+              attributes {
+                src: url
+              }
+            }
+          }
+          developers {
+            data {
+              attributes {
+                name
+              }
+            }
+          }
+          publisher {
+            data {
+              attributes {
+                name
+              }
+            }
+          }
+          categories {
+            data {
+              attributes {
+                name
+              }
+            }
+          }
+          platforms {
+            data {
+              attributes {
+                name
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
