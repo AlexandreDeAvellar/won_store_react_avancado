2401ac9d40cd057503ec4399e80d607a1ed51d04
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2023-06-28 14:13:29 -0300

    feat: create query QUERY_RECOMMENDED

---
+++ src/graphql/queries/recommended.tsx
@@ -0,0 +1,28 @@
+import { gql } from '@apollo/client'
+import { GameFragment, HighlightFragment } from '../../graphql/fragments'
+
+export const QUERY_RECOMMENDED = gql`
+  ${HighlightFragment}
+  ${GameFragment}
+  query QueryRecommended {
+    recommended {
+      data {
+        attributes {
+          section {
+            title
+            highlight {
+              ...HighlightFragment
+            }
+            games {
+              data {
+                attributes {
+                  ...GameFragment
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
