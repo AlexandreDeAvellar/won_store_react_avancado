2edb22e4a3e596d8e498b6ce2c23507dad09bfa3
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2023-06-28 14:15:13 -0300

    feat: create query QUERY_UPCOMING

---
+++ src/graphql/queries/upcoming.ts
@@ -0,0 +1,30 @@
+import { gql } from '@apollo/client'
+import { GameFragment } from 'graphql/fragments/game'
+import { HighlightFragment } from 'graphql/fragments/highlight'
+
+export const QUERY_UPCOMING = gql`
+  ${GameFragment}
+  ${HighlightFragment}
+
+  query QueryUpcoming($date: Date!) {
+    upcomingGames: games(filters: { release_date: { gte: $date } }, sort: "release_date:desc", pagination: { limit: 10 }) {
+      data {
+        attributes {
+          ...GameFragment
+        }
+      }
+    }
+    showcase: home {
+      data {
+        attributes {
+          upcomingGamesHighlight: upcoming {
+            title
+            highlight {
+              ...HighlightFragment
+            }
+          }
+        }
+      }
+    }
+  }
+`
