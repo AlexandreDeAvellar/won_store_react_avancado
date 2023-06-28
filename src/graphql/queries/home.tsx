480f8ead9565bbbf32967a4a731a3c064c0df0e4
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2023-06-28 07:33:17 -0300

    feat: create graphql QUERY_HOME

---
+++ src/graphql/queries/home.tsx
@@ -0,0 +1,78 @@
+import { gql } from '@apollo/client'
+
+import { BannerFragment, GameFragment, HighlightFragment } from '../fragments/'
+
+export const QUERY_HOME = gql`
+  ${BannerFragment}
+  ${GameFragment}
+  ${HighlightFragment}
+
+  query QueryHome($date: Date!) {
+    banners {
+      data {
+        attributes {
+          ...BannerFragment
+        }
+      }
+    }
+    newGames: games(filters: { release_date: { gte: $date } }, sort: "release_date:desc", pagination: { limit: 10 }) {
+      data {
+        attributes {
+          ...GameFragment
+        }
+      }
+    }
+    upcomingGames: games(filters: { release_date: { gt: $date } }, sort: "release_date:asc", pagination: { limit: 10 }) {
+      data {
+        attributes {
+          ...GameFragment
+        }
+      }
+    }
+    freeGames: games(filters: { price: { eq: 0 } }, sort: "release_date:desc", pagination: { limit: 10 }) {
+      data {
+        attributes {
+          ...GameFragment
+        }
+      }
+    }
+
+    sections: home {
+      data {
+        attributes {
+          newGames {
+            title
+            highlight {
+              ...HighlightFragment
+            }
+          }
+          popularGames {
+            title
+            highlight {
+              ...HighlightFragment
+            }
+            games {
+              data {
+                attributes {
+                  ...GameFragment
+                }
+              }
+            }
+          }
+          upcoming {
+            title
+            highlight {
+              ...HighlightFragment
+            }
+          }
+          freeGames {
+            title
+            highlight {
+              ...HighlightFragment
+            }
+          }
+        }
+      }
+    }
+  }
+`
