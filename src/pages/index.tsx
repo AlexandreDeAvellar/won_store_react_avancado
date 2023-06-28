d64841c0692e3b9c0f74cdb5a78eb28c65944e23
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2023-06-28 07:38:05 -0300

    feat: apply populate props from apolloClient and use transform data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,39 @@
+import { initializeApollo } from '../utils/apollo'
 import Home, { HomeTemplateProps } from '../templates/Home'
-import { homeTemplateProps } from '../templates/Home/home-mocks'
+import { QUERY_HOME } from '../graphql/queries/home'
+import { bannerTransform, gameCardTransform, highlightTransform, sectionsTransform } from '../utils/graphql-transform'
 
 export default function Index(props: HomeTemplateProps) {
   return <Home {...props} />
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
+  const apolloClient = initializeApollo()
+  const { data } = await apolloClient.query({ query: QUERY_HOME, variables: { date: '2023-01-01' } })
+  const banners = bannerTransform(data?.banners)
+  const section = sectionsTransform(data?.sections)
+
+  const games = {
+    newGames: gameCardTransform(data?.newGames),
+    upcomingGames: gameCardTransform(data?.upcomingGames),
+    freeGames: gameCardTransform(data?.freeGames),
+    mostPopularGames: gameCardTransform(section.popularGames?.games)
+  }
+
+  const titles = {
+    freeGamesTitle: section.freeGames.title,
+    newGamesTitle: section.newGames.title,
+    popularGamesTitle: section.popularGames.title,
+    upcomingTitle: section.upcoming.title
+  }
+
+  const highlights = {
+    mostPopularHighLight: highlightTransform(section.popularGames.highlight[0]),
+    upcomingHighlight: highlightTransform(section.upcoming.highlight),
+    freeHighlight: highlightTransform(section.freeGames.highlight)
+  }
+
   return {
-    props: {
-      ...homeTemplateProps
-    }
+    props: { revalidate: 60, banners, ...games, ...titles, ...highlights }
   }
 }
