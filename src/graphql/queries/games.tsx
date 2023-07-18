306d58bde2101efe84c52420655b15e066fd4f1c
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2023-07-18 08:03:24 -0300

    feat: add pagination total in QueryGames and create useQueryGames

--- src/graphql/queries/games.tsx
+++ src/graphql/queries/games.tsx
@@ -1,11 +1,19 @@
-import { gql } from '@apollo/client'
+import { gql, useQuery } from '@apollo/client'
 import { GameFragment } from '../../graphql/fragments/game'
 
 export const QUERY_GAMES = gql`
   ${GameFragment}
 
-  query QueryGames($limit: Int!) {
-    games(pagination: { limit: $limit }) {
+  query QueryGames($limit: Int!, $start: Int, $where: GameFiltersInput, $sort: [String]) {
+    gamesConnection: games(filters: $where, sort: $sort) {
+      meta {
+        pagination {
+          total
+        }
+      }
+    }
+
+    games(pagination: { limit: $limit, start: $start }, filters: $where, sort: $sort) {
       data {
         attributes {
           ...GameFragment
@@ -14,3 +22,24 @@ export const QUERY_GAMES = gql`
     }
   }
 `
+
+type QueryGamesProps = {
+  limit: number
+  start: number
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  filters?: any
+}
+
+export const useQueryGames = ({ limit, start, filters = {} }: QueryGamesProps) => {
+  return useQuery(QUERY_GAMES, { variables: { limit, start, ...filters } })
+}
+
+// FILTER MODEL
+// {
+//   "limit": 20,
+//   "where": {
+//     "developers": { "name": { "in": "Asobo Studio" } },
+//     "platforms": {  "name": { "in": ["windows", "linux"] } },
+//     "price": { "lte": 130 }
+//   }
+// }
