0ba70abf763383fbc5336e36569415939b89b54d
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2023-08-08 12:12:54 -0300

    test: ensure useCart render items and its info if there are any in the cart

--- src/hooks/use-cart/test.tsx
+++ src/hooks/use-cart/test.tsx
@@ -1,16 +1,24 @@
-import { renderHook } from '@testing-library/react'
+import { renderHook, waitFor } from '@testing-library/react'
 import { CartProvider, CartProviderProps, useCart } from '.'
 import { setStorageItem } from '../../utils/localStorage'
+import { cartItems, useCartGamesMock } from './use-cart-mock'
+import { MockedProvider } from '@apollo/client/testing'
 
 describe('useCart', () => {
   const ids = ['1', '2']
 
-  it('should render items and its info if there are any in the cart', () => {
-    const wrapper = ({ children }: CartProviderProps) => <CartProvider>{children}</CartProvider>
+  it('should render items and its info if there are any in the cart', async () => {
+    const wrapper = ({ children }: CartProviderProps) => (
+      <MockedProvider mocks={[useCartGamesMock]}>
+        <CartProvider>{children}</CartProvider>
+      </MockedProvider>
+    )
 
     setStorageItem('cartItems', ids)
     const { result } = renderHook(() => useCart(), { wrapper })
 
-    expect(result.current.items).toStrictEqual(ids)
+    await waitFor(() => expect(result.current.items).toStrictEqual(cartItems))
+    await waitFor(() => expect(result.current.quantity).toBe(2))
+    await waitFor(() => expect(result.current.total).toBe('$38.49'))
   })
 })
---
+++ src/hooks/use-cart/use-cart-mock.ts
@@ -0,0 +1,89 @@
+import { graphql_host } from '../../utils/graphql-transform/graphql_host'
+import { QUERY_GAMES } from '../../graphql/queries/games'
+
+export const useCartGamesMock = {
+  request: {
+    query: QUERY_GAMES,
+    variables: { limit: 1000, start: 0, where: { id: { in: ['1', '2'] } } }
+  },
+  result: {
+    data: {
+      gamesConnection: {
+        meta: {
+          pagination: {
+            total: 2
+          }
+        }
+      },
+      games: {
+        data: [
+          {
+            attributes: {
+              name: 'Enter the Gungeon',
+              slug: 'enter-the-gungeon',
+              cover: {
+                data: {
+                  attributes: {
+                    url: '/uploads/enter_the_gungeon_46f4fe4c68.jpg'
+                  }
+                }
+              },
+              developers: {
+                data: [
+                  {
+                    attributes: {
+                      name: 'Dodge Roll'
+                    }
+                  }
+                ]
+              },
+              price: 27.99,
+              __typename: 'Game'
+            },
+            id: '1'
+          },
+          {
+            attributes: {
+              name: 'The Witcher: Enhanced Edition',
+              slug: 'the-witcher',
+              cover: {
+                data: {
+                  attributes: {
+                    url: '/uploads/the_witcher_fe844be564.jpg'
+                  }
+                }
+              },
+              developers: {
+                data: [
+                  {
+                    attributes: {
+                      name: 'CD PROJEKT RED'
+                    }
+                  }
+                ]
+              },
+              price: 10.5,
+              __typename: 'Game'
+            },
+            id: '2'
+          }
+        ]
+      }
+    }
+  }
+}
+
+export const cartItems = [
+  {
+    id: '1',
+    img: graphql_host + '/uploads/enter_the_gungeon_46f4fe4c68.jpg',
+    price: '$27.99',
+    title: 'Enter the Gungeon'
+  },
+  {
+    id: '2',
+    img: graphql_host + '/uploads/the_witcher_fe844be564.jpg',
+    price: '$10.50',
+    title: 'The Witcher: Enhanced Edition'
+  }
+]
