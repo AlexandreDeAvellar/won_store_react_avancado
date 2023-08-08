c43b4745d230092290745df04775d191bf0909bf
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2023-08-08 12:15:54 -0300

    feat: ensure useCart render items and its info if there are any in the cart

--- src/hooks/use-cart/index.tsx
+++ src/hooks/use-cart/index.tsx
@@ -1,13 +1,26 @@
 import { createContext, useContext, useEffect, useState } from 'react'
+import { useQueryGames } from '../../graphql/queries/games'
+
 import { getStorageItem } from '../../utils/localStorage'
+import { currencyFormat } from '../../utils/format'
+import { cartTransform } from '../../utils/graphql-transform'
 
 const CART_KEYS = 'cartItems'
 
-export type CartContextProps = {
-  items: string[]
+export type CartItem = {
+  id: string
+  img: string
+  title: string
+  price: string
 }
 
-export const defaultCartItem: CartContextProps = { items: [] }
+export type CartContextProps = {
+  items: CartItem[]
+  quantity: number
+  total: string
+}
+
+export const defaultCartItem: CartContextProps = { items: [], quantity: 0, total: '$0.00' }
 
 export const CartContext = createContext<CartContextProps>(defaultCartItem)
 
@@ -19,13 +32,23 @@ export const CartProvider = ({ children }: CartProviderProps) => {
   const [cartItems, setCartItems] = useState<string[]>([])
 
   useEffect(() => {
-    const data = getStorageItem(CART_KEYS)
-    if (data) setCartItems(data)
+    const idGames = getStorageItem(CART_KEYS)
+    if (idGames) setCartItems(idGames)
   }, [])
 
-  const items = cartItems
+  const { data } = useQueryGames({
+    limit: 1000,
+    start: 0,
+    filters: { where: { id: { in: cartItems } } }
+  })
 
-  return <CartContext.Provider value={{ items }}>{children}</CartContext.Provider>
+  const items = cartTransform(data?.games)
+  const quantity = cartItems.length
+  const total = data?.games?.data?.reduce((acc: number, game: { attributes: { price: number } }) => {
+    return acc + game.attributes.price
+  }, 0)
+
+  return <CartContext.Provider value={{ items, quantity, total: currencyFormat(total || 0) }}>{children}</CartContext.Provider>
 }
 
 export const useCart = () => useContext(CartContext)
