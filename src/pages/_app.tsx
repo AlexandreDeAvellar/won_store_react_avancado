006dba832215f7796028b5804e668272463b6460
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2023-08-23 12:56:54 -0300

    feat: apply SessionProvider to be accessed in all application

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import NextNProgress from 'nextjs-progressbar'
 
+import { SessionProvider } from 'next-auth/react'
 import { ApolloProvider } from '@apollo/client'
 import { ThemeProvider } from 'styled-components'
 import type { AppProps } from 'next/app'
@@ -15,18 +16,20 @@ export default function App({ Component, pageProps }: AppProps) {
   const client = useApollo(pageProps.initialApolloState)
 
   return (
-    <ApolloProvider client={client}>
-      <ThemeProvider theme={theme}>
-        <CartProvider>
-          <Head>
-            <title>Won Games</title>
-            <meta name="description" content="The best Game Store in the world!" />
-          </Head>
-          <NextNProgress color="#F231A5" startPosition={0.3} stopDelayMs={200} height={3} showOnShallow={true} />
-          <Component {...pageProps} />
-        </CartProvider>
-        <GlobalStyles />
-      </ThemeProvider>
-    </ApolloProvider>
+    <SessionProvider session={pageProps.session}>
+      <ApolloProvider client={client}>
+        <ThemeProvider theme={theme}>
+          <CartProvider>
+            <Head>
+              <title>Won Games</title>
+              <meta name="description" content="The best Game Store in the world!" />
+            </Head>
+            <NextNProgress color="#F231A5" startPosition={0.3} stopDelayMs={200} height={3} showOnShallow={true} />
+            <Component {...pageProps} />
+          </CartProvider>
+          <GlobalStyles />
+        </ThemeProvider>
+      </ApolloProvider>
+    </SessionProvider>
   )
 }
