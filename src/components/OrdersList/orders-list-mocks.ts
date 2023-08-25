3a1d85c10bcaa6eefda366ac683cb3d6dc6e253d
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2023-08-25 07:08:12 -0300

    test: add variable id on mocks

--- src/components/OrdersList/orders-list-mocks.ts
+++ src/components/OrdersList/orders-list-mocks.ts
@@ -3,6 +3,7 @@ import { OrdersListProps } from '.'
 export const ordersListProps: OrdersListProps = {
   items: [
     {
+      id: '1',
       img: 'https://source.unsplash.com/user/willianjusten/151x70',
       title: 'Red Dead Redemption 2',
       price: 'R$ 215,00',
@@ -15,6 +16,7 @@ export const ordersListProps: OrdersListProps = {
       }
     },
     {
+      id: '2',
       img: 'https://source.unsplash.com/user/willianjusten/151x70',
       title: 'Red Dead Redemption 2',
       price: 'R$ 215,00',
