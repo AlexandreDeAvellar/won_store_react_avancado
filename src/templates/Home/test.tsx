2f96e685253a90a174dc25e576b4ebeb0f218f39
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2023-08-25 12:13:20 -0300

    test: add jest mock template/Base

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -4,6 +4,12 @@ import { render as renderWithTheme, screen } from '../../utils/test-utils'
 
 import { homeTemplateProps } from './home-mocks'
 
+jest.mock('../../templates/Base', () => ({
+  __esModule: true,
+  default: function Mock({ children }: { children: React.ReactNode }) {
+    return <div data-testid="Mock Base">{children}</div>
+  }
+}))
 jest.mock('../../components/Showcase', () => ({ __esModule: true, default: jest.fn(() => <div data-testId={'Mock Showcase'}></div>) }))
 jest.mock('../../components/BannerSlider', () => ({ __esModule: true, default: jest.fn(() => <div data-testId={'Mock BannerSlider'}></div>) }))
 
--- src/templates/Wishlist/test.tsx
+++ src/templates/Wishlist/test.tsx
@@ -3,6 +3,12 @@ import { render as renderWithTheme, screen } from '../../utils/test-utils'
 import { gameCardProps } from '../../components/GameCardSlider/game-card-slider-mocks'
 import { highlightProps } from '../../components/Highlight/highlight-mocks'
 
+jest.mock('../../templates/Base', () => ({
+  __esModule: true,
+  default: function Mock({ children }: { children: React.ReactNode }) {
+    return <div data-testid="Mock Base">{children}</div>
+  }
+}))
 jest.mock('../../components/Showcase', () => ({ __esModule: true, default: jest.fn(() => <div data-testId="Mock Showcase"></div>) }))
 
 const props: WishlistTemplateProps = {
