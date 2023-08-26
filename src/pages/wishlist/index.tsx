2849f4d89597a123075ed0e4e92a5efb99e73133
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2023-08-26 11:14:17 -0300

    feat: apply protectedRoutesServer

--- src/pages/wishlist/index.tsx
+++ src/pages/wishlist/index.tsx
@@ -3,18 +3,22 @@ import { gameCardProps } from '../../components/GameCardSlider/game-card-slider-
 import { initializeApollo } from 'utils/apollo'
 import { QUERY_RECOMMENDED } from '../../graphql/queries/recommended'
 import { recommendedTransform } from 'utils/graphql-transform/recommended'
+import { GetServerSidePropsContext } from 'next'
+import { protectedRoutesServer } from '../../utils/protected-routes'
 
 export default function WishlistPage(props: WishlistTemplateProps) {
   return <Wishlist {...props} />
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await protectedRoutesServer(context)
   const apolloClient = initializeApollo()
   const { data } = await apolloClient.query({ query: QUERY_RECOMMENDED })
   const { recommendedTitle, recommendedGames, recommendedHighlight } = recommendedTransform(data?.recommended)
 
   return {
     props: {
+      session,
       games: gameCardProps,
       recommendedTitle,
       recommendedGames,
