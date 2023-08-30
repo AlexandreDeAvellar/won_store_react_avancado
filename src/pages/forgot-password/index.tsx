c0fa5cba31dde313cc20c733deb5ddac60f712ec
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2023-08-30 12:26:44 -0300

    feat: create ForgotPassword page

---
+++ src/pages/forgot-password/index.tsx
@@ -0,0 +1,6 @@
+import FormForgotPassword from '../../components/FormForgotPassword'
+import Auth from '../../templates/Auth'
+
+const ForgotPassword = () => <Auth title="Request new password">{<FormForgotPassword />}</Auth>
+
+export default ForgotPassword
