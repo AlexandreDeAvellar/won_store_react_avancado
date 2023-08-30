1b29b1d0c9a1901e0c708e7c9f1b074821be62e2
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2023-08-30 12:52:02 -0300

    feat: create ResetPassword page

---
+++ src/pages/reset-password/index.tsx
@@ -0,0 +1,6 @@
+import FormResetPassword from '../../components/FormResetPassword'
+import Auth from 'templates/Auth'
+
+const ResetPassword = () => <Auth title="Reset password">{<FormResetPassword />}</Auth>
+
+export default ResetPassword
