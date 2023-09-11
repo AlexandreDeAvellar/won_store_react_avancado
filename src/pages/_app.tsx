e5e1904deea479974d3debed79a7e29549753d6f
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2023-09-11 13:18:01 -0300

    feat: add WishlistProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,7 @@ import GlobalStyles from 'styles/global'
 import { useApollo } from '../utils/apollo'
 import 'styles/globals.css'
 import { CartProvider } from '../hooks/use-cart'
+import { WishlistProvider } from '../hooks/use-wishlist'
 
 export default function App({ Component, pageProps }: AppProps) {
   const client = useApollo(pageProps.initialApolloState)
@@ -20,12 +21,14 @@ export default function App({ Component, pageProps }: AppProps) {
       <ApolloProvider client={client}>
         <ThemeProvider theme={theme}>
           <CartProvider>
-            <Head>
-              <title>Won Games</title>
-              <meta name="description" content="The best Game Store in the world!" />
-            </Head>
-            <NextNProgress color="#F231A5" startPosition={0.3} stopDelayMs={200} height={3} showOnShallow={true} />
-            <Component {...pageProps} />
+            <WishlistProvider>
+              <Head>
+                <title>Won Games</title>
+                <meta name="description" content="The best Game Store in the world!" />
+              </Head>
+              <NextNProgress color="#F231A5" startPosition={0.3} stopDelayMs={200} height={3} showOnShallow={true} />
+              <Component {...pageProps} />
+            </WishlistProvider>
           </CartProvider>
           <GlobalStyles />
         </ThemeProvider>
