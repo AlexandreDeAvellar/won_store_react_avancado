0315a2b368d632d046df8ced49f22e52f11d43b9
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2023-09-11 08:31:01 -0300

    test: ensure useWishlist get, create and update wishlist

---
+++ src/hooks/use-wishlist/test.tsx
@@ -0,0 +1,74 @@
+import { MockedProvider } from '@apollo/client/testing'
+import { renderHook, waitFor } from '@testing-library/react'
+
+import { useWishlist, WishlistContextProviderProps, WishlistProvider } from '.'
+import { mockCreateWishList, mockGetWishlist, mockUpdateWishlist, mockUpdateWishlistItems, mockWishlistItems } from './use-wishlist-mock'
+import { act } from 'react-dom/test-utils'
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const mockUseSession = jest.spyOn(require('next-auth/react'), 'useSession')
+const mockSession = { jwt: 'hashed_id', user: { email: 'mail@mail.com', id: '3' } }
+mockUseSession.mockImplementation(() => ({ data: mockSession }))
+
+describe('useWishlist', () => {
+  it('should return wishlist items', async () => {
+    const wrapper = ({ children }: WishlistContextProviderProps) => {
+      return (
+        <MockedProvider mocks={[mockGetWishlist]}>
+          <WishlistProvider>{children}</WishlistProvider>
+        </MockedProvider>
+      )
+    }
+    const { result } = renderHook(() => useWishlist(), { wrapper })
+
+    expect(result.current.loading).toBe(true)
+    await waitFor(() => expect(result.current.items).toStrictEqual(mockWishlistItems))
+  })
+
+  it('should check if the game is in the wishlist', async () => {
+    const wrapper = ({ children }: WishlistContextProviderProps) => (
+      <MockedProvider mocks={[mockGetWishlist]}>
+        <WishlistProvider>{children}</WishlistProvider>
+      </MockedProvider>
+    )
+
+    const { result } = renderHook(() => useWishlist(), { wrapper })
+    await waitFor(() => expect(result.current.isInWishlist('1')).toBe(true))
+    await waitFor(() => expect(result.current.isInWishlist('2')).toBe(true))
+    await waitFor(() => expect(result.current.isInWishlist('3')).toBe(false))
+  })
+
+  it('should add item in wishlist creating new list', async () => {
+    const wrapper = ({ children }: WishlistContextProviderProps) => {
+      return (
+        <MockedProvider mocks={[mockCreateWishList]}>
+          <WishlistProvider>{children}</WishlistProvider>
+        </MockedProvider>
+      )
+    }
+
+    const { result } = renderHook(() => useWishlist(), { wrapper })
+
+    await waitFor(() => expect(result.current.loading).toBe(true))
+    await act(() => result.current.addWishlist('1'))
+    await waitFor(() => expect(result.current.items).toStrictEqual([mockWishlistItems[0]]))
+  })
+
+  it('should add item in wishlist updating the current list', async () => {
+    const wrapper = ({ children }: WishlistContextProviderProps) => {
+      return (
+        <MockedProvider mocks={[mockGetWishlist, mockUpdateWishlist]}>
+          <WishlistProvider>{children}</WishlistProvider>
+        </MockedProvider>
+      )
+    }
+
+    const { result } = renderHook(() => useWishlist(), { wrapper })
+
+    await waitFor(() => expect(result.current.loading).toBe(true))
+    await waitFor(() => expect(result.current.items.length).toBe(2))
+    await act(() => result.current.addWishlist('3'))
+    await waitFor(() => expect(result.current.items.length).toBe(3))
+    await waitFor(() => expect(result.current.items).toStrictEqual(mockUpdateWishlistItems))
+  })
+})
---
+++ src/hooks/use-wishlist/use-wishlist-mock.ts
@@ -0,0 +1,95 @@
+import { graphql_host } from '../../utils/graphql-transform/graphql_host'
+import { mockUseCartItems } from '../../hooks/use-cart/use-cart-mock'
+import { QUERY_WISHLIST } from '../../graphql/queries/wishlist'
+import { MUTATION_CREATE_WISHLIST, MUTATION_UPDATE_WISHLIST } from '../../graphql/mutations/wishlist'
+
+const get_result = {
+  data: {
+    wishlists: {
+      data: [
+        {
+          id: '1',
+          attributes: {
+            games: mockUseCartItems
+          }
+        }
+      ]
+    }
+  }
+}
+
+const create_result = {
+  data: {
+    createWishlist: {
+      data: {
+        id: '1',
+        attributes: {
+          games: { data: [mockUseCartItems.data[0]] }
+        }
+      }
+    }
+  }
+}
+
+export const update_result = {
+  data: {
+    updateWishlist: {
+      data: {
+        id: '1',
+        attributes: {
+          games: { data: [...mockUseCartItems.data, { attributes: { ...mockUseCartItems.data[0].attributes, name: 'Novo Jogo' }, id: '3' }] }
+        }
+      }
+    }
+  }
+}
+
+export const mockGetWishlist = {
+  request: {
+    query: QUERY_WISHLIST,
+    context: { session: { jwt: 'hashed_id' } },
+    variables: { identifier: 'mail@mail.com' }
+  },
+  result: get_result
+}
+
+export const mockCreateWishList = {
+  request: {
+    query: MUTATION_CREATE_WISHLIST,
+    context: { session: { jwt: 'hashed_id' } },
+    variables: { user: '3', games: ['1'] }
+  },
+  result: create_result
+}
+
+export const mockUpdateWishlist = {
+  request: {
+    query: MUTATION_UPDATE_WISHLIST,
+    context: { session: { jwt: 'hashed_id' } },
+    variables: { id: '1', data: { user: '3', games: ['1', '2', '3'] } }
+  },
+  result: update_result
+}
+
+export const mockWishlistItems = [
+  {
+    id: '1',
+    title: 'Enter the Gungeon',
+    slug: 'enter-the-gungeon',
+    developer: 'Dodge Roll',
+    img: graphql_host + '/uploads/enter_the_gungeon_46f4fe4c68.jpg',
+    price: '$27.99',
+    promotionalPrice: '$27.99'
+  },
+  {
+    id: '2',
+    title: 'The Witcher: Enhanced Edition',
+    slug: 'the-witcher',
+    developer: 'CD PROJEKT RED',
+    img: graphql_host + '/uploads/the_witcher_fe844be564.jpg',
+    price: '$10.50',
+    promotionalPrice: '$10.50'
+  }
+]
+
+export const mockUpdateWishlistItems = [...mockWishlistItems, { ...mockWishlistItems[0], title: 'Novo Jogo', id: '3' }]
