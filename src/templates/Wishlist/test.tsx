33c741b3d4b896e4ba7222bacf3a26107709cbb7
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2023-09-29 13:00:21 -0300

    test: ensure <Wishlist /> get game items from wishlist context provider

--- src/templates/Wishlist/test.tsx
+++ src/templates/Wishlist/test.tsx
@@ -3,6 +3,7 @@ import Wishlist, { WishlistTemplateProps } from '.'
 import { render as renderWithTheme, screen } from '../../utils/test-utils'
 import { gameCardProps } from '../../components/GameCardSlider/game-card-slider-mocks'
 import { highlightProps } from '../../components/Highlight/highlight-mocks'
+import { WishlistContextDefault } from '../../hooks/use-wishlist'
 
 jest.mock('../../templates/Base', () => ({
   __esModule: true,
@@ -13,7 +14,6 @@ jest.mock('../../templates/Base', () => ({
 jest.mock('../../components/Showcase', () => ({ __esModule: true, default: jest.fn(() => <div data-testId="Mock Showcase"></div>) }))
 
 const props: WishlistTemplateProps = {
-  games: gameCardProps,
   recommendedGames: gameCardProps,
   recommendedHighlight: highlightProps,
   recommendedTitle: 'You may like these games'
@@ -21,14 +21,15 @@ const props: WishlistTemplateProps = {
 
 describe('<Wishlist />', () => {
   it('should render Heading and Showcase correctly', () => {
-    renderWithTheme(<Wishlist {...props} />)
+    renderWithTheme(<Wishlist {...props} />, { wishlistProviderProps: { ...WishlistContextDefault, items: gameCardProps } })
 
     expect(screen.getByRole('heading', { name: /wishlist/i })).toBeInTheDocument()
     expect(screen.getByTestId('Mock Showcase')).toBeInTheDocument()
     expect(screen.getAllByText(/population zero/i)).toHaveLength(6)
   })
+
   it('should render empty when there are no games', () => {
-    renderWithTheme(<Wishlist {...props} games={[]} />)
+    renderWithTheme(<Wishlist {...props} />, { wishlistProviderProps: { ...WishlistContextDefault } })
 
     expect(screen.queryByText(/population zero/i)).not.toBeInTheDocument()
 
