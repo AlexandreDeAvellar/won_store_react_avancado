dbb42b066fca24629977239e91e3a14b55688b34
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2023-09-29 12:58:21 -0300

    feat: config initializeApollo to receive section, get wishlist with session user and set initialApolloState to extract cache from apolloClient

--- src/pages/wishlist/index.tsx
+++ src/pages/wishlist/index.tsx
@@ -5,6 +5,7 @@ import { QUERY_RECOMMENDED } from '../../graphql/queries/recommended'
 import { recommendedTransform } from 'utils/graphql-transform/recommended'
 import { GetServerSidePropsContext } from 'next'
 import { protectedRoutesServer } from '../../utils/protected-routes'
+import { QUERY_WISHLIST, QueryWishlistProps, QueryWishlistVariables } from '../../graphql/queries/wishlist'
 
 export default function WishlistPage(props: WishlistTemplateProps) {
   return <Wishlist {...props} />
@@ -12,12 +13,21 @@ export default function WishlistPage(props: WishlistTemplateProps) {
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   const session = await protectedRoutesServer(context)
-  const apolloClient = initializeApollo()
+  const apolloClient = initializeApollo(null, session)
+
+  if (!session) return {}
+
   const { data } = await apolloClient.query({ query: QUERY_RECOMMENDED })
   const { recommendedTitle, recommendedGames, recommendedHighlight } = recommendedTransform(data?.recommended)
 
+  await apolloClient.query<QueryWishlistProps, QueryWishlistVariables>({
+    query: QUERY_WISHLIST,
+    variables: { identifier: session?.user?.email as string }
+  })
+
   return {
     props: {
+      initialApolloState: apolloClient.cache.extract(),
       session,
       games: gameCardProps,
       recommendedTitle,
