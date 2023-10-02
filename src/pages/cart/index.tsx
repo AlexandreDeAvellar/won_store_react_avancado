840b2f0eb9da6d8d9667b149a0afa4e3be6c336f
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2023-10-02 13:14:02 -0300

    feat: add protectedRoutesServer in page Cart

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -5,18 +5,22 @@ import { paymentCardProps } from '../../components/PaymentOptions/payment-option
 import { initializeApollo } from '../../utils/apollo'
 import { recommendedTransform } from '../../utils/graphql-transform/recommended'
 import { QUERY_RECOMMENDED } from '../../graphql/queries/recommended'
+import { protectedRoutesServer } from '../../utils/protected-routes'
+import { GetServerSidePropsContext } from 'next'
 
 export default function CartPage(props: CartProps) {
   return <Cart {...props} />
 }
 
-export async function getServerSideProps() {
-  const apolloClient = initializeApollo()
+export async function getServerSideProps(ctx: GetServerSidePropsContext) {
+  const session = await protectedRoutesServer(ctx)
+  const apolloClient = initializeApollo(null, session)
   const { data } = await apolloClient.query({ query: QUERY_RECOMMENDED })
   const { recommendedTitle, recommendedGames, recommendedHighlight } = recommendedTransform(data?.recommended)
 
   return {
     props: {
+      session,
       items: gameItemProps,
       total: '$ 430,00',
       cards: paymentCardProps,
