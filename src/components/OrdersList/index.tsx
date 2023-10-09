1c9210aeb9ec40b06370c27d3154f5f95d8280e1
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2023-10-09 12:54:17 -0300

    feat: update OrdersListProps

--- src/components/OrdersList/index.tsx
+++ src/components/OrdersList/index.tsx
@@ -1,10 +1,16 @@
 import Empty from '../Empty'
-import GameItem, { GameItemProps } from '../GameItem'
+import GameItem, { GameItemProps, PaymentInfoProps } from '../GameItem'
 import Heading from '../Heading'
 import * as S from './styles'
 
+type OrderProps = {
+  id: string
+  paymentInfo: PaymentInfoProps
+  games: GameItemProps[]
+}
+
 export type OrdersListProps = {
-  items?: GameItemProps[]
+  items?: OrderProps[]
 }
 
 const OrdersList = ({ items = [] }: OrdersListProps) => (
@@ -14,7 +20,9 @@ const OrdersList = ({ items = [] }: OrdersListProps) => (
     </Heading>
 
     {items.length ? (
-      items.map((item) => <GameItem key={item.downloadLink} {...item} />)
+      items.map((order) => {
+        return order.games.map((game) => <GameItem key={order.id} {...game} paymentInfo={order.paymentInfo} />)
+      })
     ) : (
       <Empty title="You have no orders yet" description="Go back to the store and explore great games and offers" hasLink />
     )}
