a30d1572cee2e069fab4cddf4369d8783160b406
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2023-10-09 08:01:08 -0300

    feat: create Query_Orders

---
+++ src/graphql/queries/oders.ts
@@ -0,0 +1,44 @@
+import { gql } from '@apollo/client'
+import { GameFragment } from '../fragments'
+import { GameCardGraphqlProps } from '../../utils/graphql-transform'
+
+export type QueryOrdersProps = {
+  orders: {
+    data: [
+      {
+        id: string
+        attributes: {
+          createdAt: string
+          card_brand: string
+          card_last4: string
+          games: GameCardGraphqlProps
+        }
+      }
+    ]
+  }
+}
+
+export const Query_Orders = gql`
+  ${GameFragment}
+
+  query QueryOrders($identifier: ID!) {
+    orders(filters: { user: { id: { eq: $identifier } } }) {
+      data {
+        id
+        attributes {
+          createdAt
+          card_brand
+          card_last4
+          games {
+            data {
+              id
+              attributes {
+                ...GameFragment
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
