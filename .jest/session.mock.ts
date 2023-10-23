46389a03ec1ba640d031eb736439776c8f9f12af
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2023-10-23 08:49:03 -0300

    test: transform session.mock in module exporting a empty object

--- .jest/session.mock.ts
+++ .jest/session.mock.ts
@@ -1,3 +1,5 @@
 const mockUseSession = jest.spyOn(require('next-auth/react'), 'useSession')
 const mockSession = { jwt: 'hashed_id', user: { email: 'mail@mail.com', id: '3' } }
 mockUseSession.mockImplementation(() => ({ data: mockSession }))
+
+export {}
